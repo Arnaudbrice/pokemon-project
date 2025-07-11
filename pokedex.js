090871ac09af1b36a1d83fe7c6eb6b6ade87fc34
Marvin Wolff <m.w@nr1.io>
2025-07-11 11:40:27 +0200

    first commit pokedex2

--- pokedex.js
+++ pokedex.js
@@ -114,3 +114,19 @@ const searchPokemon = (allPokemon) => {
     searchResult.classList.add("hidden");
   });
 };
+// ---------------- display Pokemon---------------
+const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+console.log(favorites);
+favorites.forEach((pokemon) => {
+  fetch(pokemon.url)
+    .then((response) => response.json())
+    .then((data) => {
+      const card = document.createElement("div");
+      card.className = "bg-white p-4 rounded shadow";
+      card.innerHTML = `
+  <img src="${data.sprites.front_default}" alt="${data.name}" class="mx-auto" />
+  <h2 class="text-center mt-2">${data.name}</h2>
+`;
+      document.getElementById("pokedexGrid").appendChild(card);
+    });
+});
